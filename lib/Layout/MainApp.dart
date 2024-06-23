4691132d9f62da41fb578be8e5a4b5de1256ffdb
reem <hatemreem37@gmail.com>
2024-06-23 03:26:49 +0300

    reem

--- lib/Layout/MainApp.dart
+++ lib/Layout/MainApp.dart
@@ -10,55 +10,58 @@ import 'package:playhub/screens/SearchScreen/search.dart';
 class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) => AppCubit(),
-      child: BlocConsumer<AppCubit, AppStates>(
-        listener: (BuildContext context, AppStates state) {},
-        builder: (BuildContext context, AppStates state) {
-          return Scaffold(
-            body: AppCubit.get(context)
-                .pages[AppCubit.get(context).currentScreenIdx],
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => Search()));
-              },
-              child: Icon(Icons.search, color: AppColors.white,),
-              backgroundColor: AppColors.darkGreen,
-              // label: Text('Browse all courts'),
-            ),
-            floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-            bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              currentIndex: AppCubit.get(context).currentScreenIdx,
-              onTap: (index) {
-                AppCubit.get(context).changeScreenIdx(index);
-              },
-              backgroundColor: AppColors.green3,
-              selectedItemColor: AppColors.white,
-              unselectedItemColor: AppColors.darkGray,
-              items: [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: "Home",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.groups),
-                  label: "Rooms",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.sports_soccer),
-                  label: "Booking",
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.account_circle),
-                  label: "Profile",
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+    return BlocConsumer<AppCubit, AppStates>(
+      listener: (BuildContext context, AppStates state) {},
+      builder: (BuildContext context, AppStates state) {
+        return Scaffold(
+          body: AppCubit.get(context)
+              .pages[AppCubit.get(context).currentScreenIdx],
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              AppCubit.get(context).searchFunction().then((_){
+                if(AppCubit.get(context).state is AppChangeSearchFunction){
+                  Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Search()));
+                }
+                
+              });
+              
+            },
+            child: Icon(Icons.search, color: AppColors.white,),
+            backgroundColor: AppColors.darkGreen,
+            // label: Text('Browse all courts'),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: AppCubit.get(context).currentScreenIdx,
+            onTap: (index) {
+              AppCubit.get(context).changeScreenIdx(index);
+            },
+            backgroundColor: AppColors.green3,
+            selectedItemColor: AppColors.white,
+            unselectedItemColor: AppColors.darkGray,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.groups),
+                label: "Rooms",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.sports_soccer),
+                label: "Booking",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                label: "Profile",
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
