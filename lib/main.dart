a467bea3d804b2d08ea6417373c8b716162e1e97
Doha Ahmed <dohaahmed656@gmail.com>
2024-06-28 21:01:39 +0300

    Doha

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class MyApp extends StatelessWidget {
             builder: (_, child) {
               log("userDataInMain: ${LocalStorage().userData}");
               return MaterialApp(
-                locale: const Locale('ar'),
+                locale: const Locale('en'),
                 localizationsDelegates: const [
                   S.delegate,
                   GlobalMaterialLocalizations.delegate,
