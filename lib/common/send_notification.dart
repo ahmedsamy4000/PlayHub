07f045b59f8a9519bbff29979bd1ac0e8f98526a
merafahmishokri <mera.fahmy219@gmail.com>
2024-06-30 05:08:04 +0300

    Mera

--- lib/common/send_notification.dart
+++ lib/common/send_notification.dart
@@ -1,36 +1,64 @@
-// import 'dart:convert';
-//
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
-//
-// sendAndRetrieveMessage(String title, String body,) async {
-//   await firebaseMessaging.requestPermission(
-//       sound: true, badge: true, alert: true, provisional: false);
-//   await http.post(
-//     Uri.parse('https://fcm.googleapis.com/fcm/send'),
-//     headers: <String, String>{
-//       'Content-Type': 'application/json',
-//       'Authorization': 'key=$serverToken',
-//     },
-//     body: jsonEncode(
-//       <String, dynamic>{
-//         'notification': <String, dynamic>{
-//           'body': body,
-//           'title': title,
-//           'sound': "assets/sound/notification.mp3",
-//           'default_sound': true,
-//           'default_vibrate_timings': true,
-//           'default_light_settings': true,
-//         },
-//         'priority': 'high',
-//         'data': <String, dynamic>{
-//           'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-//         },
-//         "to": "/topics/all",
-//         //'to': token,
-//       },
-//     ),
-//   );
-// }
+import 'dart:convert';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/auth_io.dart' as auth;
+
+final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+
+const String projectId = 'playhub-e2ca5';
+const String firebaseAdminScope = 'https://www.googleapis.com/auth/firebase.messaging';
+
+Future<void> sendAndRetrieveMessage(String title, String body, BuildContext context) async {
+  await firebaseMessaging.requestPermission(
+    sound: true,
+    badge: true,
+    alert: true,
+    provisional: false,
+  );
+
+  final serviceAccount = jsonDecode(await DefaultAssetBundle.of(context).loadString('assets/playhub-e2ca5-firebase-adminsdk-inped-df527cddbd.json'));
+
+  final auth.ServiceAccountCredentials credentials = auth.ServiceAccountCredentials.fromJson(serviceAccount);
+
+  final auth.AutoRefreshingAuthClient client = await auth.clientViaServiceAccount(credentials, [firebaseAdminScope]);
+
+  final response = await client.post(
+    Uri.parse('https://fcm.googleapis.com/v1/projects/$projectId/messages:send'),
+    headers: <String, String>{
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode(
+      <String, dynamic>{
+        'message': {
+          'notification': {
+            'title': title,
+            'body': body,
+          },
+          'android': {
+            'notification': {
+              'sound': 'default',
+            },
+          },
+          'apns': {
+            'payload': {
+              'aps': {
+                'sound': 'default',
+              },
+            },
+          },
+          'data': {
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+          },
+          'topic': 'all',
+        },
+      },
+    ),
+  );
+
+  if (response.statusCode == 200) {
+    print('Message sent successfully.');
+  } else {
+    print('Failed to send message: ${response.body}');
+  }
+}
